808abf563a9fec1e78a0fe434e16e26248d8f9df
SkiiDuu <drewaga3@gmail.com>
2025-08-28 01:17:35 -0500

    basic League Data added to teamstotals

--- js/data.js
+++ js/data.js
@@ -1,15 +1,24 @@
 const teamsTotals = [];
+const shortYears = [];
 
 async function initTeamsTotals() {
-    for (let i = 0; i < numOfTeams + 1; i++) {
-        teamsTotals.push({rosterId: i + 1});
+    for (let i = 0; i < numOfTeams; i++) {
+        teamsTotals.push({rosterId: i + 1, fpts: 0, fpts_against: 0, wins: 0, losses: 0});
     }
 }
 
-async function getTotalsData() {
+async function getTotalsData() { 
     for (let i = 0; i < years.length; i++) {
-        for (let j = 0; J < leagueSettings[i]["total_rosters"]; j++) {
-            //this function is gonna be fun to write...
+        if (leagueSettings[years[i]]["total_rosters"] < numOfTeams) {//check to see if each year is a shorter year so we can later fill in this year for the extra teams with average to make data more balanced
+            shortYears.push(years[i], (numOfTeams - leagueSettings[years[i]]["total_rosters"]));
+        }
+        for (let j = 0; j < leagueSettings[years[i]]["total_rosters"]; j++) {
+            if (!isNaN(teams[j][years[i]]["metadata"]["fpts_against"])) {
+                teamsTotals[j]["fpts_against"] += teams[j][years[i]]["metadata"]["fpts_against"];
+            }            
+            teamsTotals[j]["fpts"] += teams[j][years[i]]["metadata"]["fpts"];
+            teamsTotals[j]["wins"] += teams[j][years[i]]["metadata"]["wins"];
+            teamsTotals[j]["losses"] += teams[j][years[i]]["metadata"]["losses"];
         }
     }
 }
@@ -21,9 +30,11 @@ async function main() {
     await fetchUserData();
     await initTeamsTotals();
     await fetchLeagueData();
+    await getTotalsData();
 }
 
 main();
 console.log(teamsTotals);
 console.log(teams);
 console.log(leagueSettings);
+console.log(shortYears);
